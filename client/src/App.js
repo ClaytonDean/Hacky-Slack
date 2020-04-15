949d33e3f2c90a136d0924a3caad48b7275f96e1
Boyan Xu <dominictxu1@gmail.com>
2020-04-15 10:10:05 -0700

    backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
-import React from 'react';
-
+import React, { useContext } from 'react'; // <-- updated
 import Chat from './components/Chat/Chat';
 import Join from './components/Join/Join';
+import 'bulma/css/bulma.css';
+import { Auth0Context } from './contexts/auth0-context'; 
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
+function App() {
+  const auth0 = useContext(Auth0Context);
 
 const App = () => {
   return (
-    <Router>
-      <Route path="/" exact component={Join} />
-      <Route path="/chat" component={Chat} />
-    </Router>
+    <div className="hero is-info is-fullheight">
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <h1>Click Below!</h1>
+          <button onClick={auth0.loginWithRedirect} className="button is-danger">
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
-
+}
 export default App;
