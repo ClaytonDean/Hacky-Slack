944e4600f76e440f65feace2df3ac6348e569e63
Clayton Dean <clay_holman8@yahoo.com>
2020-04-17 11:13:41 -0700

    navbar styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar/NavBar";
 import Chat from './components/Chat/Chat';
 import Join from './components/Join/Join';
 import Splash from './components/splashPage/splash';
